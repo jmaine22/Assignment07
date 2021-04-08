0fe0d92ff1de0cf34d8f2abd54f2fb06d6223f89
Jmaine22 <jmaine22@gmail.com>
2021-04-07 21:19:11 -0700

    Assignment07

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
-
+let myEmployee = function (){
+    "use strict";
+     window.document.getElementById(addForm);
+     window.document.getElementById(employees);
+}
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-
+let employeeCount;
 
 // ADD EMPLOYEE
-form.addEventListener('submit', (e) => {
+let myForm = form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault();
     // GET THE VALUES FROM THE TEXT BOXES
-
+    e.document.getElementById('id').value;
+    e.document.getElementById('name').value;
+    e.document.getElementById('extension').value;
+    e.document.getElementById('email').value;
+    e.document.getElementById('department').value;
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
@@ -18,11 +26,19 @@ form.addEventListener('submit', (e) => {
     // CREATE THE DELETE BUTTON
 
     // RESET THE FORM
+    e.myEmployee.addForm.reset();
 
     // SET FOCUS BACK TO THE ID TEXT BOX
 
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+     employeeCount += 1;
 
 });
-
 // DELETE EMPLOYEE
+
+window.addEventListener("load", function () {
+    "use strict";
+    myEmployee();
+    myForm();
+
+})
